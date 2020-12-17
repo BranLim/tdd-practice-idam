6d82bff6647cd2738e6988910797edfa2eb43c24
Brandon Lim <brandon.lim@zuhlke.com>
2020-12-17 22:17:04 +0800

    Add test for user application service.

---
+++ src/test/java/com/zuhlke/authenticator/application/UserApplicationServiceTest.java
@@ -0,0 +1,11 @@
+package com.zuhlke.authenticator.application;
+
+import org.junit.jupiter.api.Test;
+
+public class UserApplicationServiceTest {
+
+    @Test
+    public void RegisteredUserLoginSucceed(){
+        UserApplicationService applicationService = new UserApplicationService();
+    }
+}
