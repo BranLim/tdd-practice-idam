4b965151396ab03efc37a848884db5ad6ecb2614
Brandon Lim <limws.brandon@gmail.com>
2020-12-18 21:58:27 +0800

    Refactor user MFA test

--- src/test/java/com/zuhlke/idam/domain/UserTest.java
+++ src/test/java/com/zuhlke/idam/domain/UserTest.java
@@ -1,19 +1,20 @@
 package com.zuhlke.idam.domain;
 
 import com.zuhlke.idam.infrastructure.persistence.MockUserRepository;
+import com.zuhlke.idam.infrastructure.services.OTPGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
 
     @Test
-    public void setupUser2FASucceed() {
+    public void setupUserMFASucceed() {
 
         UserRepository userRepository = new MockUserRepository();
 
         User user = new User(userRepository.nextId(), "TestUser1", "TestUser1@domain.com", "TestPassword");
-        String totpKeyUri = user.setup2FA("example", new UserService(userRepository));
-        Assertions.assertTrue(totpKeyUri.contains("otpauth://totp/example%3ATestUser1@domain.com?secret="));
+        MFAResult totpKeyUri = user.setupMFA("example", new PasswordService(), new OTPGenerator());
+        Assertions.assertTrue(totpKeyUri.getKeyUri().contains("otpauth://totp/example%3ATestUser1@domain.com?secret="));
     }
 
     @Test
