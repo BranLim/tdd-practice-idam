37b52d665d1fa6c2e9a6fabde33dc13892fe5bc5
Brandon Lim <limws.brandon@gmail.com>
2020-12-25 22:00:28 +0800

    Rename test method

--- src/test/java/com/zuhlke/idam/infrastructure/services/QRCodeGeneratorTest.java
+++ src/test/java/com/zuhlke/idam/infrastructure/services/QRCodeGeneratorTest.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class QRCodeGeneratorTest {
 
     @Test
-    public void generateTotpQRCodeSucceed() {
+    public void successWhenTotpQRCodeIsGenerated() {
         String totpKeyUri = "otpauth://totp/ZuhlkeTest%3Atestuser1@ZuhlkeTest.com?secret=PJ2WQ3DLMVSW24DPO5SXE2LOM5UWIZLB&issuer=ZuhlkeTest";
         int width = 256;
         int height = 256;
